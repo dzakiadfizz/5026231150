45fdfb5069de30ace268dcb6bc85e90b8abf486f
dzakiadfizz <adfiz.dzaki@gmail.com>
2025-05-21 09:53:01 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@ Route::get('halo', function () {
 Route::get('blog', function () {
 	return view('blog');
 });
+
+use App\Http\Controllers\Dosencontroller;
+Route::get('/dosen', [DosenController::class, 'index']);
+
+use App\Http\Controllers\Pegawaicontroller;
+Route::get('/pegawai', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('formulir/proses', [PegawaiController::class, 'proses']);
+
+use App\Http\Controllers\BlogController;
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
